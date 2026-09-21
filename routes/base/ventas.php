b3296e76da8794130e3842ca4f1fe101f74d6c4f
ynavarrete8181 <p1311718181@dn.uleam.edu.ec>
2026-09-20 23:16:21 -0500

    Expone detalle de venta para comprobante

--- routes/base/ventas.php
+++ routes/base/ventas.php
@@ -20,6 +20,7 @@ Route::middleware(['base.auth'])->prefix('ventas')->group(function (): void {
 
     Route::middleware(['base.permiso:VENTAS-VENTAS'])->group(function (): void {
         Route::get('ventas', [VentaControlador::class, 'ventas'])->name('ventas.ventas.index');
+        Route::get('ventas/{id}/detalle', [VentaControlador::class, 'detalleVenta'])->name('ventas.ventas.detalle');
         Route::get('pos/contexto', [VentaControlador::class, 'contextoPos'])->name('ventas.pos.contexto');
         Route::post('pos', [VentaControlador::class, 'guardarVentaPos'])->name('ventas.pos.store');
         Route::post('pos/cobrar', [VentaControlador::class, 'cobrarVentaPos'])->name('ventas.pos.cobrar');
